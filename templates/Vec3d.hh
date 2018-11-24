c457ec7609a33b5749ecfc7bab8622bcba90ba6c
Justin Trugman <jtrugman14@gmail.com>
2018-11-24 12:32:09 -0500

    Vec3d Template

--- templates/Vec3d.hh
+++ templates/Vec3d.hh
@@ -1,6 +1,25 @@
 #pragma once
 
-class Vec3d {
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+template <class Vec>
 
+class Vec3d{
+private:
+  Vec x = 0, y = 0, z = 0;
+
+public:
+    Vec3d(Vec xInput, Vec yInput, Vec zInput) : x(xInput), y(yInput), z(zInput) {}
+
+    Vec3d operator +(Vec3d right) const{
+        return Vec3d(right.x + x, right.y + y, right.z + z);
+    }
+
+  
+    friend ostream& operator <<(ostream& s, Vec3d xyz ) {
+        s << xyz.x << ", " << xyz.y << ", " << xyz.z << '\n';
+    return s;
+    }
 };
