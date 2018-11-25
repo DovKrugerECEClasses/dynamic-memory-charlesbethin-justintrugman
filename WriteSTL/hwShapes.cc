2cc9cad151f478f7f992e77fa10622a582497618
Charles Bethin <cbethin@stevens.edu>
2018-11-24 22:22:06 -0500

    shapes update

--- WriteSTL/hwShapes.cc
+++ WriteSTL/hwShapes.cc
@@ -5,16 +5,19 @@
 https://en.wikipedia.org/wiki/STL_(file_format)
 	
  */
+#include "Shapes.hh"
 
 int main() {
 	Design d;
 	double s = 10;
 	Cube c(s);
+
+	double x, y, z = 10;
 	c.translate(x,y,z);
 	// rotate??
 	cout << c.volume() << '\n';
 	c.write("out.stl"); // how to get all my shapes into the same file
-  d.add(c);
+  	d.add(c);
 	double r = 10, height  = 20;
 	Cylinder cyl(r, height);
 	d.add(c);
