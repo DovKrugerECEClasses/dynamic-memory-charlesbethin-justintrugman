298af2a35291f9ab3721dcca9c614ab463e64247
Justin Trugman <jtrugman14@gmail.com>
2018-11-26 14:00:48 -0500

    Fixed Template Functions Sum & print

--- templates/templatefunctions.cc
+++ templates/templatefunctions.cc
@@ -4,20 +4,20 @@
 #include <algorithm>
 using namespace std;
 
-template <class FIRST, class SECOND>
-FIRST sum(FIRST num, SECOND size){
-	FIRST s = 0;
-	for (int i = 0; i <= 1 - size; i++){
-		s += num[i];
+template <typename T>
+T sum(T x[], int size){
+	T s = 0;
+
+	for (int i = 0; i < size; i++){
+		s = s + x[i];
 	}
 	return s;
 }
 
-template <class FIRST, class SECOND>
-void print(FIRST array, SECOND size){
-	for (int i = 0; i<= 1-size; i++){
-		cout << array[i] << '\t';
-	}
+template <typename T>
+void print(T x[], int n) {
+	for (int i = 0; i < n; i++)
+		cout << x[i] << ' ';
 	cout << '\n';
 }
 
@@ -31,17 +31,17 @@ void sort(T x[], int size) {
 
 int main() {
 	int a[] = {5, 4, 3, 1, 7, 8, 9, 2, 1};
-	//sort(a, sizeof(a)/sizeof(int));
+	sort(a, sizeof(a)/sizeof(int));
 	print(a, sizeof(a)/sizeof(int));
 	double b[] = {5.5, 4.5, 1.2, 2.9, 9.2, 5.2};
-	//sort(b, sizeof(b)/sizeof(double));
+	sort(b, sizeof(b)/sizeof(double));
 	print(b, sizeof(b)/sizeof(double));
 
-	//cout << sum(a, sizeof(a)/sizeof(int)) << '\n';
-	//cout << sum(b, sizeof(b)/sizeof(double)) << '\n';
+	cout << sum(a, sizeof(a)/sizeof(int)) << '\n';
+	cout << sum(b, sizeof(b)/sizeof(double)) << '\n';
 
 	string c[] = {"yo", "ho", "test", "foo", "bar"};
-	//sort(c, sizeof(c)/sizeof(string));
+	sort(c, sizeof(c)/sizeof(string));
 	print(c, sizeof(c)/sizeof(string));
 	//TODO?	cout << sum(c, sizeof(c)/sizeof(string)) << '\n';
 }
